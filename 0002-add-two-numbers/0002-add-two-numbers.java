a7a2269de089fe53cb96abc877808de69b005b39
Marcos Sánchez <marcos_sanchezlara@comunidad.unam.mx>
2026-03-31 21:39:02 -0600

    Time: 1 ms (99.99%), Space: 46.5 MB (69.02%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode();
+        ListNode result = listNode;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {   
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            int num = sum % 10;
+            listNode.next = new ListNode(num);
+            listNode = listNode.next;
+            carry = sum / 10;
+        }
+        return result.next;
+    }
+}
